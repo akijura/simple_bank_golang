c9fe9c19b484a4ac6efffe18507b7b536bdf152a
akbarjon <juraev.a@hamkorbank.uz>
2023-05-22 01:26:32 -0400

    initial changes

---
+++ Makefile
@@ -0,0 +1,21 @@
+postgres:
+	docker run --name postgres15.3 -p 5411:5432 -e POSTGRES_USER=root -e POSTGRES_PASSWORD=secret -d postgres:15.3-alpine
+
+createdb:
+	docker exec -it postgres15.3 createdb --username=root --owner=root simple_bank
+
+dropdb:
+	docker exec -it postgres15.3 dropdb simple_bank
+
+migrateup:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5411/simple_bank?sslmode=disable" --verbose up
+
+migratedown:
+	migrate -path db/migration -database "postgresql://root:secret@localhost:5411/simple_bank?sslmode=disable" --verbose down
+
+sqlc:
+	sqlc generate
+test:
+	go test -v -cover ./...
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test
---
+++ db/sqlc/entry_test.go
@@ -0,0 +1,60 @@
+package db
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+)
+
+func createRandomEntry(t *testing.T) Entry {
+	account := createRandomAccount(t)
+	arg := CreateEntryParams{
+		AccountID: account.ID,
+		Amount:    account.Balance,
+	}
+	entry, err := testQueries.CreateEntry(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry)
+
+	require.Equal(t, arg.AccountID, account.ID)
+	require.Equal(t, arg.Amount, account.Balance)
+
+	require.NotZero(t, entry.ID)
+	require.NotZero(t, entry.CreatedAt)
+
+	return entry
+}
+
+func TestCreatedEntry(t *testing.T) {
+	createRandomEntry(t)
+}
+
+func TestGetEntry(t *testing.T) {
+	entry1 := createRandomEntry(t)
+	entry2, err := testQueries.GetEntry(context.Background(), entry1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, entry2)
+
+	require.Equal(t, entry1.ID, entry2.ID)
+	require.Equal(t, entry1.AccountID, entry2.AccountID)
+	require.Equal(t, entry1.Amount, entry2.Amount)
+	require.WithinDuration(t, entry1.CreatedAt, entry2.CreatedAt, time.Second)
+}
+
+func TestListEntries(t *testing.T) {
+	entry := createRandomEntry(t)
+	arg := ListEntriesParams{
+		AccountID: entry.AccountID,
+		Limit:     5,
+		Offset:    5,
+	}
+	entries, err := testQueries.ListEntries(context.Background(), arg)
+	require.NoError(t, err)
+	//require.Len(t, entries, 5)
+
+	for _, entry := range entries {
+		require.NotEmpty(t, entry)
+	}
+}
